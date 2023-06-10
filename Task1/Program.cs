79da8aa3f570a5326e02d21406f5c93ecc6fe42c
Denis <denis-topalov@yandex.ru>
2023-06-10 14:30:49 +1100

    Seminar_9. Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void Number(int Num)
+{
+    if (Num < 1) return;
+    Console.Write(Num + " ");
+    Number(Num - 1);
+}
+
+Number(ReadInt("Задайте число"));
+
